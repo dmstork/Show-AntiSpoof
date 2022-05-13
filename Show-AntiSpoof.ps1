2999bb32b64fe7ee0fe145393bb6260da53e6ee4
dmstork <d.stork@outlook.com>
2022-05-13 17:50:35 +0200

    Rewritten to functions, PoC

--- Show-AntiSpoof.ps1
+++ Show-AntiSpoof.ps1
@@ -150,54 +150,41 @@ If (($DomainName -eq "") -and ($DomainBatchFile -eq "")){
 Function Get-MX {
     Param($CheckDomain)
 
-        # Check MX record
-        Try {
-            $MXRecords = Resolve-DnsName -Server $DNSServer -Type MX -Name $CheckDomain -DNSOnly -ErrorAction Stop
-            $MXNumber = ($MXRecords).Count
-            $MXcounter=1
+    # Check MX record
+    Try {
+        $MXRecords = Resolve-DnsName -Server $DNSServer -Type MX -Name $CheckDomain -DNSOnly -ErrorAction Stop
+        $MXNumber = ($MXRecords).Count
+        $MXcounter=1
     
-            $DefaultColor = $host.ui.RawUI.ForegroundColor
-            $host.ui.RawUI.ForegroundColor = "Magenta"
-            Return Write-Output "Number of MX Records: $MXNumber"
-            $host.ui.RawUI.ForegroundColor = $DefaultColor 
+        $DefaultColor = $host.ui.RawUI.ForegroundColor
+        $host.ui.RawUI.ForegroundColor = "Magenta"
+        Write-Output "Number of MX Records: $MXNumber"
+        $host.ui.RawUI.ForegroundColor = $DefaultColor 
     
-            ForEach ($MXRecord in $MXRecords) {
-                $MXNameExchange = $MXRecord.NameExchange
-                $MXPreference = $MXRecord.Preference
-                $MXTTL = $MXRecord.TTL
+        ForEach ($MXRecord in $MXRecords) {
+            $MXNameExchange = $MXRecord.NameExchange
+            $MXPreference = $MXRecord.Preference
+            $MXTTL = $MXRecord.TTL
 
-                If ($null -ne $MXNameExchange) {
-                    $DefaultColor = $host.ui.RawUI.ForegroundColor
-                    $host.ui.RawUI.ForegroundColor = "Magenta"
-                    Return Write-Output "MX$MXCounter targets $MXNameExchange with preference $MXPreference and TTL $MXTTL"
-                    $host.ui.RawUI.ForegroundColor = $DefaultColor 
-                }
-                $MXcounter++
+            If ($null -ne $MXNameExchange) {
+                $DefaultColor = $host.ui.RawUI.ForegroundColor
+                $host.ui.RawUI.ForegroundColor = "Magenta"
+                Write-Output "MX$MXCounter targets $MXNameExchange with preference $MXPreference and TTL $MXTTL"
+                $host.ui.RawUI.ForegroundColor = $DefaultColor 
             }
-        } Catch {
-            $DefaultColor = $host.ui.RawUI.ForegroundColor
-            $host.ui.RawUI.ForegroundColor = "Red"
-            $ErrorMessage = $_.Exception.Message
-            Return Write-Output "MX lookup failure: $ErrorMessage"
-            $host.ui.RawUI.ForegroundColor = $DefaultColor     
+            $MXcounter++
         }
+    } Catch {
+        $DefaultColor = $host.ui.RawUI.ForegroundColor
+        $host.ui.RawUI.ForegroundColor = "Red"
+        $ErrorMessage = $_.Exception.Message
+        Write-Output "MX lookup failure: $ErrorMessage"
+        $host.ui.RawUI.ForegroundColor = $DefaultColor     
     }
+}
 
-
-ForEach ($AcceptedDomain in $AcceptedDomains) {
-    
-    # DomainOption 2 is the only single domain from cmdline, which has no header. So this is a workaround.
-    If ($DomainOption -ne 2){
-        $AcceptedDomain = $AcceptedDomain.DomainName
-    }
-
-    Write-Output ""
-    Write-Output "==============="
-    Write-Output "Checking domain $AcceptedDomain"
-    Write-Output "==============="
-    
-    # Get the MX record(s) via Function
-    Get-MX -CheckDomain $AcceptedDomain
+Function Get-SPF {
+    Param($CheckDomain)
 
     # Check SPF record
     Try {
@@ -224,7 +211,11 @@ ForEach ($AcceptedDomain in $AcceptedDomains) {
         Write-Output "SPF lookup failure: $ErrorMessage"
         $host.ui.RawUI.ForegroundColor = $DefaultColor
     }
-    
+}
+
+Function Get-DMARC {
+    Param($CheckDomain)
+
     # Check DMARC record
     Try {
         $DMARCDomain = "_dmarc."+$AcceptedDomain
@@ -243,6 +234,10 @@ ForEach ($AcceptedDomain in $AcceptedDomains) {
         Write-Output $ErrorMessage
         $host.ui.RawUI.ForegroundColor = $DefaultColor
     }
+}
+
+Function Get-DKIM {
+    Param($CheckDomain)
 
     # Check DKIM record
     Try {
@@ -264,6 +259,11 @@ ForEach ($AcceptedDomain in $AcceptedDomains) {
         Write-Output $DKIMResult
         $host.ui.RawUI.ForegroundColor = $DefaultColor
     }
+
+}
+
+Function Get-KnownDKIMSelectors {
+    Param($CheckDomain)
 
     # Check-KnownDKIMSelectors
     Try {
@@ -313,9 +313,13 @@ ForEach ($AcceptedDomain in $AcceptedDomains) {
             Write-Output " No custom selector $DKIMDomainSelector present"
         }
     }
+}
 
-    # Check-MTA-STS
-    Try {
+Function Get-MtaSts {
+    Param($CheckDomain)
+
+       # Check-MTA-STS
+       Try {
         $MTASTSDomain = "_mta-sts."+$AcceptedDomain
         $MTASTSRecord = Resolve-DnsName -Server $DNSServer -Type TXT -Name $MTASTSDomain -Dnsonly -ErrorAction Stop
         $MTASTSString = $MTASTSRecord.Strings
@@ -357,6 +361,10 @@ ForEach ($AcceptedDomain in $AcceptedDomains) {
         }
 
     }
+}
+
+Function Get-TlsRpt {
+    Param($CheckDomain)
 
     # Check TLS-RPT
     Try {
@@ -376,8 +384,26 @@ ForEach ($AcceptedDomain in $AcceptedDomains) {
         Write-Output $ErrorMessage
         $host.ui.RawUI.ForegroundColor = $DefaultColor
     }
-    
+}
 
+ForEach ($AcceptedDomain in $AcceptedDomains) {
+    
+    # DomainOption 2 is the only single domain from cmdline, which has no header. So this is a workaround.
+    If ($DomainOption -ne 2){
+        $AcceptedDomain = $AcceptedDomain.DomainName
+    }
+
+    Write-Output ""
+    Write-Output "==============="
+    Write-Output "Checking domain $AcceptedDomain"
+    Write-Output "==============="
+    Get-MX -CheckDomain $AcceptedDomain
+    Get-SPF -CheckDomain $AcceptedDomain
+    Get-DMARC -CheckDomain $AcceptedDomain
+    Get-DKIM -CheckDomain $AcceptedDomain
+    Get-KnownDKIMSelectors -CheckDomain $AcceptedDomain
+    Get-MtaSts -CheckDomain $AcceptedDomains
+    Get-MtaSts -CheckDomain $AcceptedDomains
 }
 
 
